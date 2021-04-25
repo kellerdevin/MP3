6a4c940c8f50326c50ca8263a87d70ced80a10d3
Devin Keller <dkeller@chapman.edu>
2021-04-25 12:59:04 -0700

    Added Simulation.java

---
+++ SimulationDriver.java
@@ -0,0 +1,22 @@
+public class SimulationDriver {
+    public static void main(String args[]) {
+      Card card1 = new Card(1,1);
+      //System.out.println(card1.toString());
+      Deck deck1 = new Deck();
+      //System.out.println(deck1.toString());
+      //System.out.println(deck1.deal());
+      //System.out.println(deck1.toString());
+      Player player1 = new Player();
+      Player player2 = new Player();
+      //System.out.println(deck1);
+      System.out.println(player1);
+      System.out.println(player2);
+      //System.out.println(deck1);
+      //System.out.println(player2.takeTurn());
+      //System.out.println(player2);
+      //Game game = new Game();
+      //System.out.println(game.play());
+
+
+    }
+}
